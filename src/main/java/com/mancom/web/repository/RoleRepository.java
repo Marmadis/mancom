d8c04f930f47dd823ef0e26f0518bb97f4237645
Askar Ryspayev <askarryspaev04@gmail.com>
2024-01-13 23:51:53 +0600

    registration part 1

---
+++ src/main/java/com/mancom/web/repository/RoleRepository.java
@@ -0,0 +1,7 @@
+package com.mancom.web.repository;
+
+import com.mancom.web.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+}
---
+++ src/main/java/com/mancom/web/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.mancom.web.repository;
+
+import com.mancom.web.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
